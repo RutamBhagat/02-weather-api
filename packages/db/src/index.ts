a85539e30566a6f8f5c696406880f6bd03e1e0e6
Rutam Bhagat <rutambhagat@gmail.com>
2025-12-28 12:06:10 +0530

    initial commit

---
+++ packages/db/src/index.ts
@@ -0,0 +1,9 @@
+import { env } from "@02-weather-api/env/server";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../prisma/generated/client";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
